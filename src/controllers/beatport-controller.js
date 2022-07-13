17ab80a045c3faf0f0844f82b896f084b52cb182
Garrett Gordon <gagordon@mit.edu>
2022-07-13 02:22:38 -0400

    frontend working with the beatport backend

--- src/controllers/beatport-controller.js
+++ src/controllers/beatport-controller.js
@@ -1,5 +1,18 @@
 import { BEATPORT_BACKEND_URL } from '../config'
 
 export const addSongsToBeatPortCart = async (songs) =>{
+  const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: process.env.REACT_APP_TEST_BEATPORT_USERNAME,
+          password: process.env.REACT_APP_TEST_BEATPORT_PASSWORD,
+          searches: songs.join("%20")
+        })
+    };
+
+  const result = await fetch(BEATPORT_BACKEND_URL + "/addToCart", requestOptions)
+                          .then(response => response.json())
+  console.log(result)
 
 }
